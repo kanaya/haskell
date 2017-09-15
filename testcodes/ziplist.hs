44ba04e9d5f93056b6b91ded94ee13335fa21e3e
Ichi Kanaya <kanaya@pineapple.cc>
2017-09-15 19:53:38 +0900

    ziplist *****

--- testcodes/ziplist.hs
+++ testcodes/ziplist.hs
@@ -4,15 +4,13 @@ newtype  ZipList' a = ZipList' { getList :: [a] } deriving Show
 instance Functor ZipList' where
   fmap f (ZipList' xs) = ZipList' (map f xs)
 instance Applicative ZipList' where
-  ZipList' fs <*> ZipList' xs = ZipList' (zipWith ($) fs xs) -- (fs ***** xs)
+  ZipList' fs <*> ZipList' xs = ZipList' (fs ***** xs) -- (zipWith ($) fs xs)
   pure x                      = ZipList' (repeat x)
 
--- repeat   :: [a] -> [a]
--- repeat x =  x : repeat x 
-
-(*****) _ []          = []
-(*****) [] _          = []
-(*****) (f:fs) (x:xs) = (f $ x) : fs ***** xs
+(*****)               :: [(a -> b)] -> [a] -> [b]
+(*****) _ []          =  []
+(*****) [] _          =  []
+(*****) (f:fs) (x:xs) =  (f $ x) : fs ***** xs
 
 f   :: Int -> (Int -> Int)
 f n =  \x -> x+n
